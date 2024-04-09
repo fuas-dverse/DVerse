be5463de6c18c4af6ce79a613dac62767a5783f5
Arenco Meevissen <479450@student.fontys.nl>
2024-04-09 21:26:11 +0200

    Added HSTS, X-Content-Type-Options, X-XSS-Protection and Referrer-Policy.

--- Prototype 2/frontend/next.config.mjs
+++ Prototype 2/frontend/next.config.mjs
@@ -10,6 +10,22 @@ const nextConfig = {
               key: 'Permissions-Policy',
               value: 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(self), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(self), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(self), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
             },
+            {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=63072000; includeSubDomains; preload'
+            },
+            {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff'
+            },
+            {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block'
+            },
+            {
+                key: 'Referrer-Policy',
+                value: 'origin-when-cross-origin'
+            },
           ],
         },
       ];
