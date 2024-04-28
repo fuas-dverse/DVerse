111f52b1fd092a0d2778a02704cad330c1273d86
Reno Muijsenberg <490412@student.fontys.nl>
2024-04-28 12:53:23 +0200

    Update server and add classifier

--- Prototype 3/websocket_server/server.py
+++ Prototype 3/websocket_server/server.py
@@ -1,26 +1,40 @@
 import json
 import flask
 from flask_socketio import SocketIO
+from confluent_kafka import Producer
 
+# Create a Flask application
 app = flask.Flask(__name__)
+
+# Create a SocketIO instance
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+# Create a Kafka producer
+producer = Producer({'bootstrap.servers': 'host.docker.internal:9092'})
 
-# Function to handle travel output messages
+
 def handle_output(message):
-    # Send message back to the UI
+    """
+    Handle the output from the agents to send back to UI over websocket connection.
+
+    Parameters:
+    message (str): The message to be sent back to the UI.
+    """
+
     socketio.emit('message', {'data': json.loads(message.value().decode('utf-8'))})
 
 
 @socketio.on('message')
-def handle_message(data):
-    # Retrieve the conversation_manager from Flask's application context
-    conversation_manager = flask.current_app.config['conversation_manager']
+def handle_message(message):
+    """
+    Handle incoming messages from chat screen UI to be sent to agents via Kafka.
 
-    # Send the user input to the ConversationContextManager for processing
-    conversation_manager.classify_and_route(data)
+    Parameters:
+    message (str): The message sent by the user via UI chat interface.
+    """
+    producer.produce('classifier.input', value=message.encode('utf-8'))
+    producer.flush()
 
 
 if __name__ == "__main__":
-    # Start the application
     socketio.run(app, port=5000, debug=True)
