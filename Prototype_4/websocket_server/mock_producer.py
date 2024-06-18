5cd7aaf39cc6f8d414b17d609120afdc90abed67
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2024-06-18 10:22:16 +0200

    Changed mockproducer

--- Prototype_4/websocket_server/mock_producer.py
+++ Prototype_4/websocket_server/mock_producer.py
@@ -4,16 +4,43 @@ from kafka_manager.kafka_manager import KafkaManager
 
 if __name__ == "__main__":
     kafka_manager = KafkaManager()
+    # message = {
+    #     "@context": "https://www.w3.org/ns/activitystreams",
+    #     "@type": "Object",
+    #     "actor": "agent",
+    #     "content": [
+    #         {
+    #             "type": "text",
+    #             "value": "Response as array",
+    #         },
+    #         {
+    #             "type": "text",
+    #             "value": "Blah blah blah2",
+    #         },
+    #         {
+    #             "type": "text",
+    #             "value": "Blah blah blah4",
+    #         },
+    #     ],
+    #     "chatId": "umtufgmv8gp70sdpps1d6d",
+    # }
+
     message = {
         "@context": "https://www.w3.org/ns/activitystreams",
         "@type": "Object",
         "actor": "agent",
-        "content": {
-            "type": "text",
-            "value": "Blah blah blah",
-        },
-        "chatId": "3xgprmypgweag8c01ub7cs",
+        "content": [
+            {
+                "type": "text",
+                "value": "Say hello to the world",
+            },
+            {
+                "type": "img",
+                "value": "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+        ],
+        "chatId": "i2ievobccqdp4h1qcidqbb",
     }
     message = json.dumps(message)
-    kafka_manager.producer.produce("google-search.output",  message)
+    kafka_manager.producer.produce("google-search.output", message)
     kafka_manager.producer.flush()
